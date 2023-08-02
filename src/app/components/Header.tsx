430628593e1212686d8b1b25e21b09662b36e45f
unknown <nicolas.degouveia@gmail.com>
2023-08-02 08:55:14 +0200

    remove style

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="flex justify-center px-4 py-4 border-b-2 border-b-orange-200">
+        <div className="flex justify-center px-4 py-4">
             <h2 className="text-xl font-bold lg:text-4xl">Dashboard</h2>
         </div>
     );
--- src/app/components/charts/Card.tsx
+++ src/app/components/charts/Card.tsx
@@ -16,7 +16,7 @@ function Card({ number, loading }: Props) {
         <div className=" m-auto p-4 rounded-2xl shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] bg-gradient-to-bl from-[#000] via-[#000] to-orange-200 max-w-xs h-40">
             <div className="flex flex-col items-center justify-center h-full">
                 <h3 className="text-white uppercase">Total Activities</h3>
-                {loading || number.length === 0 ? (
+                {loading || number?.length === 0 ? (
                     <div className="mt-2">
                         <Loader />
                     </div>
